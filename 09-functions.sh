595faab1442e5118c1b8b8830fc6e477be480fc7
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-04-19 17:41:52 +0530

    new commit

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,21 +4,27 @@ USERID=$(id -u) # to find the user id / -u for user
 Timestamp=$(date +%F-%H-%M-%S)
 FileName=$(echo $0 |cut -d "." -f1)
 Logfile=/tmp/$FileName-$Timestamp.log
+R='\e[0;31m'
+G='\e[0;32m'
+Y='\e[0;33m'
+Bl='\e[0;34m'
+NC='\e[0m'
+echo -e " Script started at: $Y $Timestamp $NC"
 status (){
     if [ $1 -ne 0 ]; then
-        echo "$2 installation.... failed"
+        echo -e "$bl $2 $NC installation.... $R failed $NC"
         exit 1
     else
-        echo " $2 installation..... sucessed"
+        echo -e " $bl $2 $NC installation..... $G sucessed $NC"
     fi
 
 }
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run with sudo user"
+    echo -e "$R Please run with sudo user $NC"
     exit 1 #manually exiting from the script
 else
-    echo "You are the super user...continuing to remaining steps"
+    echo -e "$G You are the super user $NC ...continuing to remaining steps"
 fi
 
 
---
+++ 10-colours
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define color codes
+BLACK='\e[0;30m'
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[0;33m'
+BLUE='\e[0;34m'
+PURPLE='\e[0;35m'
+CYAN='\e[0;36m'
+WHITE='\e[0;37m'
+NC='\e[0m'  # No color
+
+# Print colored text
+echo -e "${BLACK}Black Text${NC}"
+echo -e "${RED}Red Text${NC}"
+echo -e "${GREEN}Green Text${NC}"
+echo -e "${YELLOW}Yellow Text${NC}"
+echo -e "${BLUE}Blue Text${NC}"
+echo -e "${PURPLE}Purple Text${NC}"
+echo -e "${CYAN}Cyan Text${NC}"
+echo -e "${WHITE}White Text${NC}"
