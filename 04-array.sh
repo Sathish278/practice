5529241132432f22f12f2acb30849a4debbae50e
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-04-19 13:07:00 +0530

    new commit

---
+++ 04-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+God=("RAM" "Shiva" "krishna")
+
+#the size of the array is 3
+# index are 0,1,2
+
+echo " first god is : ${God[0]}"
+echo " Second god is : ${God[1]}"
+echo " Third god is : ${God[2]}"
