9091b56819785185bef9a6ac5c4506b6c21d5cc8
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-04-19 12:20:50 +0530

    new commit

---
+++ 02-argument.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declearing veriables and taking arguments/paramaters from cmd
+Per1=$1 #no space b/w variable = and name
+Per2=$2
+
+#refering variables
+echo "$Per1:: Hi $per2 how are you?"
+echo "$Per2:: Hello $Per1 I'm fine, what about you?"
+echo "$Per1:: I'm fine, who was the day?"
+echo "$Per2:: All going good....."
