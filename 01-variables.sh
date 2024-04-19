1c5cee01e9210cacdce9a75cdf4e5523bf3a80d8
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-04-19 12:08:42 +0530

    new commit

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Per1=Sathish #no space b/w variable = and name
+per2=Reddy
+
+echo "$Per1:: Hi $per2 how are you?"
+echo "$Per2:: Hello $Per1 I'm fine, what about you?"
+echo "$Per1:: I'm fine, who was the day?"
+echo "$Per2:: All going good....."
